1fdb4e05e1028a56437897394dd90150108acb74
Bryan Lunt <lunt@ctbp.ucsd.edu>
2013-07-31 02:49:02 -0700

    initial import

---
+++ getNcbiSeq.pl
@@ -0,0 +1,332 @@
+#!/usr/bin/perl -w
+use strict;
+use Getopt::Long;
+use Data::Dumper;
+use LWP::Simple;
+use DB_File;
+
+
+my ($inputSeqFile, $outputFolder, $maxSubject, $cut_e_value, $homo_range, $min_size, $max_size) = ('', 'faaOut', 1000, "1e-20", 'F', 0.7, 1.5);
+my $options = GetOptions(
+			"i=s" => \$inputSeqFile,
+			"t=s" => \$cut_e_value,
+			"o=s" => \$outputFolder,
+			"h=s" => \$homo_range,
+			"s=f" => \$min_size,
+			"l=f" => \$max_size,
+			);
+
+if (! $inputSeqFile) {
+	print "use getNcbiSeq.pl [options]\n";
+	print "\noptions\n\n";
+	print "   -i input filename in fasta format, required\n";
+	print "   -o output folder, default faaOut\n";
+	print "   -t blast threshold, default 1e-20\n";
+	print "   -h remain alignment region, [T/F], default F\n";
+	print "   -s minimal sequence size, x time bigger then original sequence, default 0.7\n";
+	print "   -l maximal sequence size, y times small then original sequences, defalut 1.5\n";
+
+	exit;
+}
+
+my $querySeqHashRef = GetInputSeqHashRef($inputSeqFile);
+
+#print Dumper($querySeqHashRef);
+my $blastOutputHashRef = Blastcl3($querySeqHashRef, $maxSubject);
+PasteSeqToFiles($blastOutputHashRef, $querySeqHashRef, $cut_e_value, $homo_range, $min_size, $max_size, $outputFolder);
+
+
+
+
+
+
+sub GetNcbiSeq {
+	my ($giArrayRef) = @_;
+	my $NcbiSeqFileName = "NCBIsequence.faaTemp";
+	my $NcbiSeqHashRef = {};
+	if (-e $NcbiSeqFileName) {
+		$NcbiSeqHashRef = SeqFileToHash($NcbiSeqFileName);
+		open (OSS, ">>$NcbiSeqFileName");
+	} else {
+		open (OSS, ">$NcbiSeqFileName");
+	}
+
+#	my @giArray;
+	foreach my $gi (@$giArrayRef) {
+#		push @giArray, $gi;
+		unless (exists $NcbiSeqHashRef->{$gi}) {
+			my $url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=protein&retmode=text&rettype=fasta&id=$gi";
+			print "Now download gi-$gi sequence\n";
+			my $sequence = get($url);
+			unless ($sequence) { $sequence = get($url)};
+			if ($sequence =~ /Error/) {
+				print "..error to download gi-$gi sequence!!\n";
+				next;
+			} else {
+				$NcbiSeqHashRef->{$gi} = $sequence;
+				print OSS $sequence;
+			}
+		}
+	}
+#	my $temp;	
+#	for (my $i = 0; $i < (@giArray); $i++) {
+#		$temp .= $giArray[$i] . ',';
+#		if (($i + 1) % 10 == 0 || $i == $#giArray) {
+#			my $url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=protein&retmode=text&rettype=fasta&id=$temp";
+#			my $sequence = get($url);
+#			print "$temp\n";
+#			$temp = '';
+#		}
+#	}
+
+	close OSS;
+	return $NcbiSeqHashRef;
+}
+
+
+
+
+
+sub PasteSeqToFiles{
+	my ($blastOutputHashRef, $querySeqHashRef, $cut_e_value, $homo_range, $min_size, $max_size, $outputFolder) = @_;
+	my $ncbiSeqHashRef = GetNcbiSeq([keys %$blastOutputHashRef]);
+	my $allNames = [keys %$querySeqHashRef];
+	my $nameGiHashRef = {};
+	my $seqBelowFilter = [];
+	while (my ($gi, $resultHashRef) = each %$blastOutputHashRef) {
+
+		if (!exists $ncbiSeqHashRef->{$gi}) {
+			delete $blastOutputHashRef->{$gi};
+			next;
+		}
+		my ($name, $e_value) = ($resultHashRef->{'n'}, $resultHashRef->{'e'});
+
+		if ($e_value < $cut_e_value) {
+			push @{$nameGiHashRef->{$name}}, $gi;
+		} else {
+			push @$seqBelowFilter, "$gi\t$name\t$e_value\n";
+		}
+	}
+	close OSS;
+
+	
+	open (OR, ">remove.table");
+	open (ORS, ">remove.faa1");
+	foreach (@$seqBelowFilter) {
+		my @token = split(/\t/,$_);
+		print ORS $ncbiSeqHashRef->{$token[0]};
+		print OR $_;
+	}
+	close OR;
+
+	unless (-e $outputFolder) {
+		system("mkdir $outputFolder");
+	}
+	
+	foreach my $name (@$allNames) {
+		open (OF,">./$outputFolder/$name.faa");
+		print OF $querySeqHashRef->{$name};
+		print OF "\n";
+		unless (exists $nameGiHashRef->{$name}) {
+			next;
+		}
+		my $giArrayRef = $nameGiHashRef->{$name};
+		my $seqTemp = {};
+		my $seqNum = (@$giArrayRef);
+		my $order = []; 
+		my $oriSeqSize = seqSize(\$querySeqHashRef->{$name});
+		my $seqSizeHash = {};
+		foreach my $gi (@$giArrayRef) {
+			my $size = seqSize(\$ncbiSeqHashRef->{$gi});
+			$seqSizeHash->{$gi} = $size;
+		}
+
+
+		#$order = [sort {QseqSize(\$NCBIseqHashRef->{$a}) <=> QseqSize(\$NCBIseqHashRef->{$b})} @$giArrayRef];
+		$order = [sort {$seqSizeHash->{$a} <=> $seqSizeHash->{$b}} @$giArrayRef];
+		open (OF,">./$outputFolder/$name.faa");
+		print OF $querySeqHashRef->{$name}; 
+		print OF "\n";
+		foreach my $gi (@$order) {
+			if ($homo_range eq 'T') {
+				my $hr = $blastOutputHashRef->{$gi};
+				my ($q_start, $q_end, $s_start, $s_end) = ($hr->{'qs'}, $hr->{'qe'}, $hr->{'ss'}, $hr->{'se'});
+				my $start = ($s_start > $q_start) ? $s_start - $q_start - 1: 0;
+				my $end = (($oriSeqSize - $q_end) < ($seqSizeHash->{$gi} - $s_end)) ? $s_end + $oriSeqSize - $q_end - 1: $seqSizeHash->{$gi} - 1;
+		#		print "q $q_start-$q_end of $oriSeqSize\ts $s_start-$s_end of $seqSizeHash->{$gi}\t$start-$end\n"; 
+				if (($end - $start) > ($oriSeqSize * $min_size)) {
+					my $seq = GetSeqRegion(\$ncbiSeqHashRef->{$gi}, $start, $end, $seqSizeHash->{$gi});
+				#print "$name $gi\t$q_start, $q_end, $oriSeqSize\t$s_start, $s_end, $seqSizeHash->{$gi}\t$start, $end\n";
+					print OF $seq;
+			#	print OF $NCBIseqHashRef->{$gi};
+				}
+			} else {
+				if (($seqSizeHash->{$gi} < $oriSeqSize * $max_size) && ($seqSizeHash->{$gi} > $oriSeqSize * $min_size)) {
+					print OF $ncbiSeqHashRef->{$gi};
+				}
+			}
+		}
+		close OF;
+
+
+	}
+}
+
+sub GetSeqRegion {
+	my ($seqRef, $start, $end, $size) = @_;
+	my @array = split(/\n+/,$$seqRef);
+	my $title = shift(@array);
+	my $join = join('',@array);	
+	my $subseq = substr($join, $start, $end - $start);
+	for (my $i = 1; $i < (length($subseq) / 71); $i++) {
+		substr $subseq, 71 * $i -1 , 0, "\n";
+	}
+	
+#	print "$title $start-$end\n$subseq\n\n";
+	return "$title $start-$end of $size\n$subseq\n\n";
+}
+
+
+sub seqSize {
+	my ($seqRef) = @_;
+	my @array = split(/\n+/,$$seqRef);
+	my $size = 0;
+	shift(@array);
+	foreach (@array) {
+	#	if ($_ =~ /\>/) { next;}
+		$size += length($_);
+	}
+	return $size;
+}
+
+sub QseqSize {
+	return length(${$_[0]}) - index(${$_[0]},"\n");
+}
+
+sub DO_CDHIT {
+
+	my ($seqTemp, $giArray, $cutOff) = @_;
+	my $command = "cd-hit -i CD_hitTemp.seq -o CD_hitTemp.cdhit -c $cutOff >/dev/null";
+	open (OC, ">CD_hitTemp.seq");
+	foreach (@$giArray) {
+		print OC $seqTemp->{$_};
+	}
+	close OC;
+	system($command);
+	my $newArray = [];
+	my $hashRef = SeqFileToHash("CD_hitTemp.cdhit");
+	system ("rm CD_hitTemp.*");
+	@$newArray = keys %$hashRef;
+	return $newArray;
+
+}
+
+sub SeqFileToHash {
+	my ($seqFileName) = @_;
+	my $seqHashRef = {};
+	my $title = '';
+	open (ISS, $seqFileName);
+	while (<ISS>) {
+		if ($_ =~ />gi\|(\d+)\|/) {
+			$title = $1;
+			if (exists $seqHashRef->{$title}) {
+				delete $seqHashRef->{$title};
+			}
+		}
+		$seqHashRef->{$title} .= $_;
+	}
+	close ISS;
+	return $seqHashRef;
+}
+
+sub Blastcl3 {
+
+	my ($seqHashRef, $maxSubject) = @_;
+	my $giBlastResultRef = {};
+	my $giToSimilar = [];
+	unless (-e "tempOut") {
+		system("mkdir tempOut");
+	}
+
+	while (my ($name, $sequence) = each %$seqHashRef) {
+		my $tempSeqFile = "./tempOut/$name.seqTemp";
+		my $tempOutFile = "./tempOut/$name.outTemp";
+		open (OS,">$tempSeqFile");
+		print OS $sequence;
+		close OS;
+		#my $command = "blastcl3 -i $tempSeqFile -o $tempOutFile -p blastp -d nr -F F -v $maxSubject -b $maxSubject -m8 -e 1e-5";
+		unless (-s $tempOutFile) {
+			my $command = "blastcl3 -i $tempSeqFile -o $tempOutFile -p blastp -d nr -F F -v $maxSubject -b $maxSubject -m8 -e 1e-5";
+			print "now blasting $name sequence\n";
+			system($command);
+		}
+		unlink($tempSeqFile);
+		my $previousGi = '';
+		open (IB, $tempOutFile);
+		while (my $temp = <IB>)  {
+			chomp $temp;
+			my @array = split(/\s+/,$temp);
+			my ($bit_score, $e_value, $identity, $q_start, $q_end, $s_start, $s_end) = ($array[11], $array[10], $array[2], $array[6], $array[7], $array[8], $array[9]);
+			my ($gi) = $array[1] =~ /gi\|(\d+)\|/;
+			my $tempRef = { 'b' => $bit_score, 'e' => $e_value, 'n' => $name, 'id' => $identity, 'qs' => $q_start, 'qe' => $q_end, 'ss' => $s_start, 'se' => $s_end};
+
+			if ($previousGi eq $gi) {    #just select fist blast result if there are multiple hits for single sequence
+				next;
+			} else {
+				$previousGi = $gi;
+			}
+
+			if ($identity > 95) {  #mark those gi that if the identity are greather then 90 percent to the query sequence
+				push @$giToSimilar, $gi;
+			}
+
+			if ((exists $giBlastResultRef->{$gi}) && ($bit_score < $giBlastResultRef->{$gi}->{'b'})) {
+			#	print "$giBlastResultRef->{$gi}->{'n'} $giBlastResultRef->{$gi}->{'b'}     $name $bit_score\n";
+			} else {
+				$giBlastResultRef->{$gi} = $tempRef;
+			}
+		}
+		
+
+	}
+
+	foreach my $gi (@$giToSimilar) { #remove those sequences that too similar to Query sequences;
+		delete $giBlastResultRef->{$gi};
+	#	print "$gi\n";
+	}
+
+	return $giBlastResultRef;
+}
+
+
+sub GetInputSeqHashRef {
+	my ($inputSeqFile) = @_;
+	my $seqHashRef = {};
+	my $seqCount = {};
+	my $currentName = '';
+	open (IS, $inputSeqFile);
+	while (<IS>) {
+		if ($_ =~ /^\s+/) { next;}
+		if ($_ =~ /\>(\S+)\s/) {
+			$currentName = $1;
+			if (length $currentName > 10) {
+				print "Sequence title $currentName is too long, it can not longer then 10 characters!!\n";
+				exit;
+			}
+			$seqCount->{$currentName}++;
+		}
+			$seqHashRef->{$currentName} .= $_;
+	}
+	my $redundantNum = 0;
+	while (my ($key, $value) = each %$seqCount) {
+		$redundantNum = ($value > $redundantNum) ? $value : $redundantNum;
+		if ($value >  1) {
+			print "sequence name $key has $value copies\n";
+		}
+	}
+	if ($redundantNum > 1) {
+		my $errmessage = "\nPlease remove redundant sequences\n\n";
+		die $errmessage;
+	}
+	return $seqHashRef;
+}
---
+++ supertree.pl
@@ -0,0 +1,275 @@
+#!/usr/bin/perl -w
+use strict;
+use Math::Complex;
+use Data::Dumper;
+use XML::Simple;
+use DB_File;
+#########################################################################
+#	Auther : Ming-Ren Yen						#
+#	Date: 18JAN08							#
+#	UCSD Saier's Lab						#
+#	To make alignment or distance matrix for superfamily tree	#
+#	from random select sequence					#
+#									#
+#########################################################################		
+
+
+# import all sequcne from all faa files to hash %infile_seq_num;
+
+my $numTrees = 100;
+my $numSeqs = 5;
+my $tempFolder = '/private/tmp/ramdisk1';
+#my $tempFolder = '/tmp/ramdisk';
+#my $tempFolder = '.';
+
+my $in = (`ls *.faa`);
+$in =~ s/.faa//g;
+my $infiles = [split /\n/,$in];
+
+
+my $seqHashRef = {};
+foreach my $inFile (@$infiles) {
+	my $hashRef = SeqFileToHash("$inFile.faa");
+	$seqHashRef->{$inFile} = $hashRef;	
+}
+
+
+open (OD,">infile") or die;
+for (my $tree = 1; $tree <= $numTrees; $tree++) {
+	my $randomHashArray = {};
+	foreach my $inFile (@$infiles) {
+		my @seqNameArray = keys %{$seqHashRef->{$inFile}};
+		my $seqNum = (@seqNameArray);
+#	$randomHashArray->{$inFile} = RandomArray(0, $seqNum - 1, $numSeqs);
+		my $tempArray = [];
+		foreach(@{RandomArray(0, $seqNum - 1, $numSeqs)}){
+#	print "$seqNameArray[$_] ";
+			push @$tempArray, $seqNameArray[$_];
+		}
+		$randomHashArray->{$inFile} = $tempArray;
+#print "\n";
+	}
+	print "tree # $tree \n";
+	my $arrayData = GetScoreMatrix($infiles, $seqHashRef, $randomHashArray);
+	my $numFiles = (@$infiles);
+	print OD "      $numFiles\n";
+	for (my $i = 0; $i < $numFiles; $i++) {
+	#	print "$infiles->[$i]\n";
+		my $outname = "$infiles->[$i]         ";
+		print OD substr($outname, 0, 10);
+		for (my $j = 0; $j < $numFiles; $j++) {
+			print OD "  $arrayData->[$i][$j]";
+		}
+		print OD "\n";
+	}
+	print OD "\n";
+
+}
+close OD;
+
+
+
+#sub MakeMatrix {
+#	my ($infiles, $seqHashRef, $randomHashArray) = @_;
+#	my $arrayData = [];
+#	my $matrixTemp = [];
+#	my $numFiles = (@$infiles);
+#
+#	push (@$matrixTemp, "      $numFiles\n");
+#	for (my $i = 0; $i < $numFiles; $i++) {
+#		my $outname = "$infiles->[$i]         ";
+#		push (@$matrixTemp, substr($outname, 0, 10));
+#               SeqToFile("$tempFolder/$infiles->[$i].subject", $seqHashRef->{$infiles->[$i]}, $randomHashArray->{$infiles->[$i]});
+#              system ("formatdb -i $tempFolder/$infiles->[$i].subject -p T -l /dev/null/log");
+#             for (my $j = 0; $j < $numFiles; $j++) {
+#    #                    if ($i < $j) {
+#                                print '.';
+#                                SeqToFile("$tempFolder/$infiles->[$j].query", $seqHashRef->{$infiles->[$j]}, $randomHashArray->{$infiles->[$j]});
+#                                my $averScore = GetScore3("$tempFolder/$infiles->[$j].query", "$tempFolder/$infiles->[$i].subject", $randomHashArray->{$infiles->[$j]}, $randomHashArray->{$infiles->[$i]});
+#                                my $middle = 1;
+#                                $arrayData->[$i][$j] = sprintf("%.6f", ($averScore));
+#				push (@$matrixTemp, "  $arrayData->[$i][$j]");
+#                                unlink("$tempFolder/$infiles->[$j].query");
+#                        } elsif ($i > $j) {
+#				push (@$matrixTemp, "  $arrayData->[$j][$i]");
+#                        } else {
+#				push (@$matrixTemp, "  0.000000");
+#                        }
+#                }
+#		push (@$matrixTemp, "\n");
+#                print "\n";
+#                unlink ("$tempFolder/$infiles->[$i].subject.phr");
+#                unlink ("$tempFolder/$infiles->[$i].subject.psq");
+#                unlink ("$tempFolder/$infiles->[$i].subject.pin");
+#                unlink ("$tempFolder/$infiles->[$i].subject");
+#        }
+#	push (@$matrixTemp, "\n");
+#        print "\n";
+#	return $matrixTemp;
+#
+#
+#}
+
+
+
+
+
+
+sub SeqFileToHash {
+	my ($seqFileName) = @_;
+	my $seqHashRef = {};
+	my $title = '';
+	open (ISS, $seqFileName);
+	while (<ISS>) {
+		if ($_ =~ />gi\|(\d+)\|/) {
+			$title = $1;
+			if (exists $seqHashRef->{$title}) {
+				delete $seqHashRef->{$title};
+			}
+		} elsif ($_ =~ />(\S+)\s/) {
+			$title = $1;
+		}
+		$seqHashRef->{$title} .= $_;
+	}
+	close ISS;
+	return $seqHashRef;
+}
+
+sub SeqToFile {
+	my ($fileName, $seqHashRef, $array) = @_;
+	open (OS1, ">$fileName");
+	foreach my $name (@$array) {
+		print OS1 $seqHashRef->{$name};
+	}
+	close OS1;
+}
+
+sub GetScoreMatrix {
+	my ($infiles, $seqHashRef, $randomHashArray) = @_;
+	my $numFiles = (@$infiles);
+	my $arrayData = [];
+	for (my $i = 0; $i < $numFiles; $i++) {
+		SeqToFile("$tempFolder/$infiles->[$i].subject", $seqHashRef->{$infiles->[$i]}, $randomHashArray->{$infiles->[$i]});
+		system ("formatdb -i $tempFolder/$infiles->[$i].subject -p T -l /dev/null/log");
+		for (my $j = 0; $j < $numFiles; $j++) {
+			if ($i < $j) {
+				print '.';
+				SeqToFile("$tempFolder/$infiles->[$j].query", $seqHashRef->{$infiles->[$j]}, $randomHashArray->{$infiles->[$j]});
+				my $averScore = GetScore3("$tempFolder/$infiles->[$j].query", "$tempFolder/$infiles->[$i].subject", $randomHashArray->{$infiles->[$j]}, $randomHashArray->{$infiles->[$i]});
+				#my $averScore = 1;
+				my $temp = sprintf("%.6f", ($averScore));
+				$arrayData->[$i][$j] = 	"$temp";
+                                unlink("$tempFolder/$infiles->[$j].query");
+                        } elsif ($i > $j) {
+				$arrayData->[$i][$j] = $arrayData->[$j][$i];
+                        } else {
+				$arrayData->[$i][$j] = "0.000000";
+                        }
+                }
+                print "\n";
+                unlink ("$tempFolder/$infiles->[$i].subject.phr");
+                unlink ("$tempFolder/$infiles->[$i].subject.psq");
+                unlink ("$tempFolder/$infiles->[$i].subject.pin");
+                unlink ("$tempFolder/$infiles->[$i].subject");
+        }
+	return $arrayData;	
+}
+
+
+
+
+
+sub GetScore3 {
+	my ($queryFile, $subjectFile, $array1, $array2) = @_;
+
+	my $scoreHash = {};
+	foreach my $name1 (@$array1) {
+		foreach my $name2 (@$array2) {
+			$scoreHash->{"$name1\t$name2"} = 10;
+		}
+	}
+	my $command = "blastall -i $queryFile -d $subjectFile -p blastp -F F -m8";
+	my @result =  `$command`;
+	foreach my $input (@result) {
+	#	print $input;
+	#	chomp $input;
+		my @token = split(/\t/, $input);
+		my ($name1) = ($token[0] =~ /gi\|(\d+)\|/) ? ($token[0] =~ /gi\|(\d+)\|/) : $token[0];
+		my ($name2) = ($token[1] =~ /gi\|(\d+)\|/) ? ($token[1] =~ /gi\|(\d+)\|/) : $token[1];
+		if ($scoreHash->{"$name1\t$name2"} == 10) {
+			$scoreHash->{"$name1\t$name2"} = $token[11];
+		}
+	#	print "$name1\t$name2\t$token[11]\n";
+	}
+	my $array = [values %$scoreHash];
+	#system ("rm formatdb.log");
+	return (100 / middleScore([values %$scoreHash])) ;
+}
+
+sub RandomArray {
+	my ($min, $max, $num) = @_;
+	my $oldArray = [$min...($max)];
+	my $size = (@$oldArray);
+	if (($max - $min) < $num) {
+	#	print "$size\t@$oldArray\n";
+		return $oldArray
+	}
+	my $newArray = [];
+	while ($num) {
+		my $rand = rand(time) * 1000 % ($size);
+		if ($oldArray->[$rand] ne 'a') {
+			push @$newArray, $oldArray->[$rand];
+			$oldArray->[$rand] = 'a';
+			$num--;
+		}
+	}
+#	print "$size\t@$newArray\n";
+	return $newArray;
+}
+
+
+sub middleScore {
+	my ($rawArrayRef) = @_;
+	my $rawArraySize = (@$rawArrayRef);
+	if ($rawArraySize < 2) {
+		return $rawArrayRef->[0];
+	} elsif ($rawArraySize < 200) {
+		my $sum = 0;
+		foreach my $score (@$rawArrayRef) {
+			$sum += $score;
+		}
+		return ($sum/$rawArraySize);
+	}
+	@$rawArrayRef = sort { $b <=> $a } @$rawArrayRef;
+	my $distributionArrayRef = [];
+	my $scale = 10; # per 1 unit of score. 
+	my $arm =  ($scale * $scale) / 2;
+	my $distributionArraySize = ($rawArrayRef->[0]) * $scale + $arm;
+
+	#initial the $distributionArrayRef
+	for (my $i = 0; $i <= $distributionArraySize; $i++) {
+		$distributionArrayRef->[$i] = 0;
+	}
+
+	
+	foreach my $score (@$rawArrayRef) {
+		my $position = $score * $scale;
+		my $cStart = $position - $arm;
+		my $cEnd = $position + $arm;;
+		for (my $i = $cStart; $i <= $cEnd; $i++) {
+			$distributionArrayRef->[$i]++;
+		}
+	}
+
+	my $highestCount = 0;
+	my $highestPosition = 10;
+	for (my $i = 0; $i <= $distributionArraySize; $i++) {
+		if ($distributionArrayRef->[$i] > $highestCount) {
+			$highestPosition = $i;
+			$highestCount = $distributionArrayRef->[$i];
+		}
+	}
+	return ($highestPosition / $scale);
+}
+
+
