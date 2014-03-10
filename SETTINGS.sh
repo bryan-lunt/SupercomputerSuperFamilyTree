ebe721e6fde92e366d5393ec884bde19c0d2cf0c
Bryan Lunt <lunt@ctbp.ucsd.edu>
2014-03-10 13:12:17 -0700

    Started breaking settings out into their own file.

---
+++ SETTINGS.sh
@@ -0,0 +1,2 @@
+export SFT_BLASTDB=${SFT_BLASTDB-'/home/blunt/bio/ncbi/db/nr'}
+export NUM_TREES=${NUM_TREES-'100'}
--- doblast.bash
+++ doblast.bash
@@ -2,10 +2,13 @@
 #PBS -q hotel
 #PBS -lnodes=1:ppn=16,walltime=10:00:00
 
+source ${SFT_BIN}/SETTINGS.sh
+
 cd $PBS_O_WORKDIR
 
 export NUM_TREES=${NUM_TREES-'100'}
 export INPUTSEQFILE=${INPUTSEQFILE-'inputfile.faa'}
+
 
 #Rename sequences so that they have names short enough to be accepted by PHYLIP.
 awk 'START {id=0;} /^>/ {id++; print ">seq_"id,$1 >"rename.txt"; print ">seq_"id; } !/^>/ {print $0; }' ${INPUTSEQFILE} > blast_input.faa
@@ -13,5 +16,5 @@ awk 'START {id=0;} /^>/ {id++; print ">seq_"id,$1 >"rename.txt"; print ">seq_"id
 
 
 set -e
-${SFT_BIN}/getNcbiSeq.pl -i blast_input.faa -N ${PBS_NUM_PPN}
+${SFT_BIN}/getNcbiSeq.pl -d ${SFT_BLASTDB} -i blast_input.faa -N ${PBS_NUM_PPN}
 exit $?
