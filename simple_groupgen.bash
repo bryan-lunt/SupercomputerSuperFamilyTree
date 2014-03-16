59575165221b70b7bd014a5b512e0709c41e1e58
Bryan Lunt <lunt@ctbp.ucsd.edu>
2014-03-15 19:54:52 -0700

    Fixed bug in simple_groupgen.bash

--- simple_groupgen.bash
+++ simple_groupgen.bash
@@ -3,4 +3,4 @@
 INFILE=$1
 OUTFILE=$(dirname ${INFILE})/groups.txt
 
-cat ${INFILE} | awk  '/^>/ { sub(/^>/,"", $1); BAZ=$1; BAZ=gensub(/(.?\..?\..?\..?)(.*)/,"\\1","1",BAZ);    print $1, BAZ;}' > ${OUTFILE}
+cat ${INFILE} | awk  '/^>/ { sub(/^>/,"", $1); BAZ=$1; BAZ=gensub(/([^\.]*\.[^\.]*\.[^\.]*\.[^\.]*)(.*)/,"\\1","1",BAZ);    print $1, BAZ;}' > ${OUTFILE}
