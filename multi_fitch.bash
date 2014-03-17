96bacc82a04412a8c405037c22c55c6732aeac32
Bryan Lunt <lunt@ctbp.ucsd.edu>
2014-03-17 12:10:16 -0700

    Fixed renaming problem.

--- multi_fitch.bash
+++ multi_fitch.bash
@@ -26,5 +26,5 @@ EOF
 cd $PBS_O_WORKDIR
 
 #name the sequences back in the tree.
-sed 's/>//g ; s/\./\\./g' rename.txt | awk '{print "s/"$1"/"$2"/g";}' > rename.sed
+sed 's/>//g ; s/\./\\./g' rename.txt | awk '{print "s/"$1":/"$2":/g";}' > rename.sed
 sed -f rename.sed consensus/outtree > FINAL_CONSENSUS_TREE.newick
